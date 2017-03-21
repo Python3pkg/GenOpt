d7e1a6f02737d35621f16ce8bdc53e4e5dad6bd1
Kevin Lioi <kevin.a.lioi@gmail.com>
2017-03-21 16:50:30 -0700

    Update setup for PyPI

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
-from setuptools import setup
+from distutils.core import setup
 setup(
     name = 'GenOpt',
     py_modules = ['GenOpt'],
-    version = '1.0.2',
+    version = '1.0.4',
     description = 'Contains a class for optimizing analytic functions utilizing a genetic algorithm.',
     author = 'Kevin Lioi',
     author_email = 'kevin.a.lioi@gmail.com',
     url = 'https://github.com/kevinlioi/GenOpt', 
     license='MIT',
-    install_requires=[
-        'numpy',
-        'random',
-        'heapq',
-        'itertools'
-    ],
-    download_url = 'https://github.com/kevinlioi/GenOpt/archive/1.0.0.tar.gz',
+    download_url = 'https://github.com/kevinlioi/GenOpt/archive/1.0.4.tar.gz',
     keywords = ['genetic', 'optimization', 'algorithm'],
-    classifiers = [],
-    
+    classifiers = []
 )
