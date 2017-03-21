40fa90dfddf24681699666e27485f5288147fda0
Kevin Lioi <kevin.a.lioi@gmail.com>
2017-03-21 16:32:27 -0700

    Updated numer of variables from example

--- examples/unconstrained_optimization.py
+++ examples/unconstrained_optimization.py
@@ -3,8 +3,6 @@ from GenOpt import GeneticOptimizer
 def StyblinskiTang_Fnc(x):
     return (x[:, 0]**4-16*x[:, 0]**2 + 5*x[:, 0])/2.0 + (x[:, 1]**4-16*x[:, 1]**2 + 5*x[:, 1])/2.0
 InitialSolutions = numpy.vstack([numpy.array([numpy.random.uniform(), 
-                                              numpy.random.uniform(), 
-                                              numpy.random.uniform(), 
                                               numpy.random.uniform()]) for i in range(1000)])
 G1 = GeneticOptimizer(InitialSolutions = InitialSolutions, 
                      Objective = StyblinskiTang_Fnc, 
