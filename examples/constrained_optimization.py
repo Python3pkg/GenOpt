e1d88a8edd4c0bbf0357fbcc10bed6a3b4268a90
Kevin Lioi <kevin.a.lioi@gmail.com>
2017-03-21 16:03:38 -0700

    Add examples

---
+++ examples/constrained_optimization.py
@@ -0,0 +1,15 @@
+import numpy
+from GenOpt import GeneticOptimizer
+##Booths Function but with an additional slack variable to show the constraint feature.
+def BoothsFnc(x):
+    return (x[:, 0] + 2*x[:, 1] - 7)**2 + (2*x[:, 0] + x[:, 1] - 5)**2
+InitialSolutions = [numpy.array([numpy.random.uniform(), numpy.random.uniform(), numpy.random.uniform()]) for i in range(1000)]
+InitialSolutions = numpy.vstack([10*z/sum(z) for z in InitialSolutions])
+G2 = GeneticOptimizer(InitialSolutions = InitialSolutions, 
+                     Objective = BoothsFnc, 
+                     ub = [10,10,10,10], 
+                     lb = [0,0,0,0], 
+                     Sense = 'min', 
+                     MutationType = 'Sum Constraint',
+                     Crossover = 'Weighted Average')
+R = G2.GA()
---
+++ examples/unconstrained_optimization.py
@@ -0,0 +1,15 @@
+import numpy
+from GenOpt import GeneticOptimizer
+def StyblinskiTang_Fnc(x):
+    return (x[:, 0]**4-16*x[:, 0]**2 + 5*x[:, 0])/2.0 + (x[:, 1]**4-16*x[:, 1]**2 + 5*x[:, 1])/2.0
+InitialSolutions = numpy.vstack([numpy.array([numpy.random.uniform(), 
+                                              numpy.random.uniform(), 
+                                              numpy.random.uniform(), 
+                                              numpy.random.uniform()]) for i in range(1000)])
+G1 = GeneticOptimizer(InitialSolutions = InitialSolutions, 
+                     Objective = StyblinskiTang_Fnc, 
+                     ub = [5,5,5,5], 
+                     lb = [-5,-5,-5,-5], 
+                     Sense = 'min', 
+                     Crossover = 'Quadratic')
+R = G1.GA()
